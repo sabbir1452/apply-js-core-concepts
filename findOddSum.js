f4728d3ea2afd08443e2a4c9528c9c29021deadd
sabbir1452 <sabbirrahman1452@gmail.com>
2024-01-15 20:08:14 +0600

    akdlkjlffjkjkkljdfs

--- findOddSum.js
+++ findOddSum.js
@@ -1,43 +1,43 @@
+// // function getSumOfAnArray(numbers) {
+// //   let sum = 0;
+// //   for (var i = 0; i < numbers.length; i++) {
+// //     const index = i;
+// //     const element = numbers[index];
+// //     console.log(element);
+// //   }
+// // }
+
+// // const myNumbers = [12, 65, 45, 78, 32, 45, 91];
+// // getSumOfAnArray(myNumbers);
+
 // function getSumOfAnArray(numbers) {
 //   let sum = 0;
-//   for (var i = 0; i < numbers.length; i++) {
+//   for (let i = 0; i < numbers.length; i++) {
 //     const index = i;
 //     const element = numbers[index];
-//     console.log(element);
+//     sum = sum + element;
+//     // console.log(index, element, sum);
 //   }
+//   return sum;
+// }
+
+// function getOddNumbersOfAnArray(numbers) {
+//   const oddNumbers = [];
+//   for (let i = 0; i < numbers.length; i++) {
+//     const index = i;
+//     const element = numbers[index];
+//     // if (element % 2 !== 0) {
+//     if (element % 2 === 1) {
+//       console.log(index, element);
+//       oddNumbers.push(element);
+//     }
+//   }
+//   return oddNumbers;
 // }
 
 // const myNumbers = [12, 65, 45, 78, 32, 45, 91];
-// getSumOfAnArray(myNumbers);
+// const oddNumbers = getOddNumbersOfAnArray(myNumbers);
+// // console.log(oddNumbers);
+// const oddNumberSum = getSumOfAnArray(oddNumbers);
 
-function getSumOfAnArray(numbers) {
-  let sum = 0;
-  for (let i = 0; i < numbers.length; i++) {
-    const index = i;
-    const element = numbers[index];
-    sum = sum + element;
-    // console.log(index, element, sum);
-  }
-  return sum;
-}
-
-function getOddNumbersOfAnArray(numbers) {
-  const oddNumbers = [];
-  for (let i = 0; i < numbers.length; i++) {
-    const index = i;
-    const element = numbers[index];
-    // if (element % 2 !== 0) {
-    if (element % 2 === 1) {
-      console.log(index, element);
-      oddNumbers.push(element);
-    }
-  }
-  return oddNumbers;
-}
-
-const myNumbers = [12, 65, 45, 78, 32, 45, 91];
-const oddNumbers = getOddNumbersOfAnArray(myNumbers);
-// console.log(oddNumbers);
-const oddNumberSum = getSumOfAnArray(oddNumbers);
-
-console.log("odd number sum", oddNumberSum);
+// console.log("odd number sum", oddNumberSum);
